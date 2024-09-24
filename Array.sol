b4e7b7558ba4091f6a8ea70bf745d5ea9598e5b8
marlisumarli <gg.marlisumarli@gmail.com>
2024-09-25 05:04:27 +0700

    Updates

---
+++ Array.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+contract ArrayOnSol{
+    function testArray() public pure returns (bytes memory) {
+        uint len = 7;
+
+        // Dynamic array
+        uint[] memory a = new uint[](7);
+
+        // Bytes is same as byte[]
+        bytes memory b = new bytes(len);
+
+        assert(a.length == 7);
+        assert(b.length == len);
+
+        // Access aray variable
+        a[6] = 8;
+
+        // Static array
+        uint[3] memory c = [uint(1), 2, 3];
+        assert(c.length == 3);
+
+        return b;
+    }
+}
---
+++ Variables.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract SolidityTest {
+    uint storedData; // State variable
+
+    constructor() public {
+        storedData = 10;
+    }
+
+    function getResult() public view returns(uint) {
+        uint a = 1; // Local variable
+        uint b = 2;
+        uint result = a + b;
+        return storedData; // Access state variable
+    }
+}
